8170651303b60e850a79b0fe515a6266bd59e280
biahsa2801 <beatrizsa.baesso_eng@hotmail.com>
2024-03-06 21:24:05 -0300

    adição e resolução de erros

--- areasGeom.py
+++ areasGeom.py
@@ -1,6 +1,15 @@
+from math import pi
+
 def area_ret (LadoA, LadoB):
 
-    area = LadoA * LadoB  
+    area = LadoA * LadoB 
 
     return area
+
+def area_circ(diametro):
+
+    area = pi * (diametro ** 2) / 4
+
+    return area
+
 
